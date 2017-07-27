ce7477c9be1019605be70cb6cfd59b5c4edb450a
jredovich <jredovich1@gmail.com>
2017-07-26 22:06:41 -0500

    Update beckk.js

--- beckk.js
+++ beckk.js
@@ -1,16 +1,32 @@
-(document).ready(function()
+$(document).ready(function()
 
 { 
     $('#text').click(function() { $(this).toggleClass('bigbeck');
     });
                              
- $('#hoe').hover(function() {
+ $('#hoe1').hover(function() {
         $(this).toggleClass('bear');    
     });
+    $('#hoe2').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ 
+ $('#hoe3').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ 
+ $('#hoe4').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+
+$('#hoe5').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ 
+ 
  
  
 
 
 
 });
-    
