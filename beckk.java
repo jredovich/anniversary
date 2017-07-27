9677863b5d1023f9bff80a63dd4780db4ca46ff3
jredovich <jredovich1@gmail.com>
2017-07-26 19:09:49 -0500

    Create beckk.java

---
+++ beckk.java
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+
+$('img').hover(function() {
+        $(this).addClass('bear');    
+    });
+  $('body').append("<p>I'm a paragraph</p>");
+    
+    });
+    
+    
