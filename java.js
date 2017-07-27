9361fe35bd41883a1677ec1ba15af64fd7d0c9d7
jredovich <jredovich1@gmail.com>
2017-07-26 22:28:35 -0500

    Create java.js

---
+++ java.js
@@ -0,0 +1,25 @@
+$(document).ready(function()
+
+{ 
+    $('#text').click(function() { $(this).toggleClass('bigbeck');
+    });
+                             
+ $('#hoe1').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+    $('#hoe2').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ 
+ $('#hoe3').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ 
+ $('#hoe4').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+
+$('#hoe5').hover(function() {
+        $(this).toggleClass('bear');    
+    });
+ });
